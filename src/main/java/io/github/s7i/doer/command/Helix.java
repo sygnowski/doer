f3993406ff63893dfed52907c1bf2a5ae0e002db
nefro85 <1815506+nefro85@users.noreply.github.com>
2021-07-16 13:29:17 +0200

    testing helix

--- src/main/java/io/github/s7i/doer/command/Helix.java
+++ src/main/java/io/github/s7i/doer/command/Helix.java
@@ -1,24 +1,18 @@
 package io.github.s7i.doer.command;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import java.util.List;
-import java.util.Map;
+import static java.util.Objects.requireNonNull;
+
+import io.github.s7i.doer.domain.helix.Controller;
+import io.github.s7i.doer.domain.helix.Participant;
+import io.github.s7i.doer.domain.helix.Spectator;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.helix.HelixManagerFactory;
-import org.apache.helix.InstanceType;
-import org.apache.helix.NotificationContext;
-import org.apache.helix.api.listeners.ExternalViewChangeListener;
-import org.apache.helix.api.listeners.IdealStateChangeListener;
-import org.apache.helix.model.ExternalView;
-import org.apache.helix.model.IdealState;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 @Command(name = "helix", description = "Runs the Helix Spectator")
 @Slf4j
-public class Helix implements Runnable, ExternalViewChangeListener, IdealStateChangeListener {
+public class Helix implements Runnable {
 
     @Option(names = "-s", defaultValue = "localhost:2181")
     String server;
@@ -29,58 +23,43 @@ public class Helix implements Runnable, ExternalViewChangeListener, IdealStateCh
     @Option(names = "-n", defaultValue = "doer")
     String instanceName;
 
-    ObjectMapper objectMapper = new ObjectMapper()
-          .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-          .configure(SerializationFeature.INDENT_OUTPUT, true);
+    @Option(names = "-t", defaultValue = "spectator")
+    String type;
 
     @SneakyThrows
     @Override
     public void run() {
-        log.info("running a helix spectator...");
-        runSpectator();
+        requireNonNull(type, "type");
+        switch (type) {
+            case "s":
+            case "spectator":
+                log.info("running a helix spectator...");
+                runSpectator();
+                break;
+            case "p":
+            case "participant":
+                log.info("running a helix participant...");
+                runParticipant();
+                break;
+            case "c":
+            case "controller":
+                log.info("running a helix controller...");
+                runController();
+                break;
+        }
+    }
+
+    private void runController() throws Exception {
+        new Controller(instanceName, clusterName, server);
+    }
+
+    private void runParticipant() throws Exception {
+        new Participant(instanceName, clusterName, server);
     }
 
     private void runSpectator() throws Exception {
-        var helix = HelixManagerFactory.getZKHelixManager(clusterName,
-              instanceName,
-              InstanceType.SPECTATOR,
-              server);
-        helix.connect();
-
-        helix.addExternalViewChangeListener(this);
-        helix.addIdealStateChangeListener(this);
+        new Spectator(instanceName, clusterName, server);
     }
 
 
-    @Override
-    public void onExternalViewChange(List<ExternalView> externalViewList, NotificationContext changeContext) {
-        try {
-            var changeType = changeContext.getChangeType();
-            var type = changeContext.getType();
-            var event = Map.of(
-                  "type", type.name(),
-                  "changeType", changeType.name(),
-                  "externalViewList", externalViewList
-            );
-            log.info("onExternalViewChange: \n{}", objectMapper.writeValueAsString(event));
-        } catch (Exception e) {
-            log.error("oops", e);
-        }
-    }
-
-    @Override
-    public void onIdealStateChange(List<IdealState> idealState, NotificationContext changeContext) throws InterruptedException {
-        try {
-            var changeType = changeContext.getChangeType();
-            var type = changeContext.getType();
-            var event = Map.of(
-                  "type", type.name(),
-                  "changeType", changeType.name(),
-                  "idealStateList", idealState
-            );
-            log.info("onIdealStateChange: \n{}", objectMapper.writeValueAsString(event));
-        } catch (Exception e) {
-            log.error("oops", e);
-        }
-    }
 }
