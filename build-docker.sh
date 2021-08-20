8f1bfa9c411093713d10b98157189429dacf3399
nefro85 <1815506+nefro85@users.noreply.github.com>
2021-08-20 17:56:23 +0200

    various Docker files build

--- build-docker.sh
+++ build-docker.sh
@@ -2,4 +2,44 @@ TAG=s7i/doer
 VERSION=0.1.1
 VCS_REF=$(git rev-parse HEAD)
 
-docker build -t $TAG:$VERSION --build-arg VERSION=$VERSION --build-arg VCS_REF=$VCS_REF .
+info() {
+    echo
+    echo Tag: $TAG:$VERSION
+    echo Git-sha: $VCS_REF
+    echo
+}
+
+with_builder () {
+    runBuild
+}
+
+slim_build () {
+    if [[ ! -e "./build/libs/doer-all.jar" ]]; then
+        ./gradlew build --console=plain
+    fi
+    runBuild "Dockerfile-slim"
+}
+
+runBuild () {
+    local dockerFile
+
+    if [[ -n "$1" ]]; then
+        dockerFile="-f $1"
+        echo "Using Dockerfile: $1"
+    else
+        echo "Using default dockerfile"
+    fi
+    
+    docker build -t $TAG:$VERSION --build-arg VERSION=$VERSION --build-arg VCS_REF=$VCS_REF $dockerFile .
+}
+
+info
+
+case $1 in
+    slim)
+        slim_build
+        ;;         
+    *)
+    with_builder        
+esac
+
