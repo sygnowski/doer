cfce2b1129d74a479231a9a6abd468e7534f3055
nefro85 <mariusz.sygnowski@gmail.com>
2022-08-27 10:16:36 +0200

    [ADD] handler to run as pipeline element

--- src/main/java/io/github/s7i/doer/command/Ingest.java
+++ src/main/java/io/github/s7i/doer/command/Ingest.java
@@ -1,6 +1,7 @@
 package io.github.s7i.doer.command;
 
 import io.github.s7i.doer.Context;
+import io.github.s7i.doer.DoerException;
 import io.github.s7i.doer.domain.ingest.IngestProcessor;
 import io.github.s7i.doer.manifest.ingest.IngestRecordManifest;
 import picocli.CommandLine.Command;
@@ -11,6 +12,18 @@ import java.util.concurrent.Callable;
 
 @Command(name = "ingest")
 public class Ingest implements Callable<Integer>, YamlParser {
+
+    public static Runnable createCommandInstance(File yaml) {
+        return () -> {
+            var ingest = new Ingest();
+            ingest.yaml = yaml;
+            try {
+                ingest.call();
+            } catch (Exception e) {
+                throw new DoerException(e);
+            }
+        };
+    }
 
     @Option(names = {"-y"}, required = true)
     File yaml;
