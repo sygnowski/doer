393afe0f6bf0fa7e701a79f6d0269cba1d8efe61
nefro85 <1815506+nefro85@users.noreply.github.com>
2022-12-02 12:22:25 +0100

    [REWORK] #22 additional options

--- src/main/java/io/github/s7i/doer/command/GrpcHealth.java
+++ src/main/java/io/github/s7i/doer/command/GrpcHealth.java
@@ -3,21 +3,33 @@ package io.github.s7i.doer.command;
 import io.github.s7i.doer.DoerException;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
+import io.grpc.StatusRuntimeException;
 import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
 import io.grpc.health.v1.HealthGrpc;
 import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 
 import java.util.concurrent.TimeUnit;
 
-@CommandLine.Command(name = "grpc-health")
+@CommandLine.Command(name = "grpc-health", description = "gRPC Health (io.grpc.HealthGrpc)")
 @Slf4j(topic = "doer.console")
 public class GrpcHealth extends Command {
 
-    @CommandLine.Parameters()
+    @CommandLine.Parameters(paramLabel = "target")
     String target;
     @CommandLine.Option(names = {"-s", "--service"}, defaultValue = "", description = "package_names.ServiceName")
     String srvName;
+
+    @CommandLine.Option(names = {"-w"}, description = "Use HealthGrpc::watch method instead HealthGrpc::check.")
+    boolean watch;
+
+    @CommandLine.Option(names = {"-n"}, description = "Number of repeated checks.", defaultValue = "0")
+    Integer noTry;
+
+    @CommandLine.Option(names = {"-d"}, description = "Delay between check in seconds", defaultValue = "1")
+    Integer delay;
+
     @Override
     public void onExecuteCommand() {
         try {
@@ -36,14 +48,44 @@ public class GrpcHealth extends Command {
             final var request = HealthCheckRequest.newBuilder()
                     .setService(srvName)
                     .build();
-            check(healthSrv, request);
+            if (watch) {
+                try {
+                    healthSrv.watch(request).forEachRemaining(this::logStatus);
+                } catch (StatusRuntimeException e) {
+                    log.warn("grpc-watch", e);
+                }
+            } else {
+                if (noTry == 0) {
+                    check(healthSrv, request);
+                } else if (noTry > 0) {
+                    repeatChecks(healthSrv, request);
+                }
+            }
         } finally {
             channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
         }
     }
 
+    private void repeatChecks(HealthGrpc.HealthBlockingStub healthSrv, HealthCheckRequest request) throws InterruptedException {
+        int n = 0;
+        do {
+            if (n != 0) {
+                TimeUnit.SECONDS.sleep(delay);
+            }
+            check(healthSrv, request);
+        } while (++n < noTry);
+    }
+
     private void check(HealthGrpc.HealthBlockingStub healthSrv, HealthCheckRequest request) {
-        final var result = healthSrv.check(request);
+        try {
+            final var result = healthSrv.check(request);
+            logStatus(result);
+        } catch (StatusRuntimeException e) {
+            log.warn("grpc-check", e);
+        }
+    }
+
+    private void logStatus(HealthCheckResponse result) {
         final var status = result.getStatus();
         final var hs = status.name();
         log.info("Health status: {}", hs);
