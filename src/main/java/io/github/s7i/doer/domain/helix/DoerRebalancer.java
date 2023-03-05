cc8e8c626e0a680b4b2610d385b6b317b54cc000
nefro85 <mariusz.sygnowski@gmail.com>
2023-03-05 14:27:06 +0100

    [ADD] Custom Rebalancer [TAG] Helix Research (#29)

---
+++ src/main/java/io/github/s7i/doer/domain/helix/DoerRebalancer.java
@@ -0,0 +1,30 @@
+package io.github.s7i.doer.domain.helix;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.helix.controller.rebalancer.DelayedAutoRebalancer;
+import org.apache.helix.controller.rebalancer.constraint.MonitoredAbnormalResolver;
+import org.apache.helix.controller.stages.CurrentStateOutput;
+import org.apache.helix.model.ClusterConfig;
+import org.apache.helix.model.IdealState;
+import org.apache.helix.model.Partition;
+import org.apache.helix.model.StateModelDefinition;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j(topic = "doer.console")
+public class DoerRebalancer extends DelayedAutoRebalancer {
+
+    @Override
+    protected Map<String, String> computeBestPossibleStateForPartition(Set<String> liveInstances, StateModelDefinition stateModelDef, List<String> preferenceList, CurrentStateOutput currentStateOutput, Set<String> disabledInstancesForPartition, IdealState idealState, ClusterConfig clusterConfig, Partition partition, MonitoredAbnormalResolver monitoredResolver) {
+        var mapping = super.computeBestPossibleStateForPartition(liveInstances, stateModelDef, preferenceList, currentStateOutput, disabledInstancesForPartition, idealState, clusterConfig, partition, monitoredResolver);
+
+        synchronized (Controller.InstanceHolder.class) {
+            mapping = Controller.InstanceHolder.getInstance().onClusterRebalance(mapping);
+        }
+
+        log.info("[HELIX|DoerRebalancer] Final Mapping: {}", mapping);
+        return mapping;
+    }
+}
