8a178dafa92b6dfdf2b06e61183cc2b66f955f96
nefro85 <mariusz.sygnowski@gmail.com>
2024-03-16 16:21:00 +0100

    [ADD] random generator (#35)

--- src/test/groovy/io/github/s7i/doer/command/KafkaFeederTest.groovy
+++ src/test/groovy/io/github/s7i/doer/command/KafkaFeederTest.groovy
@@ -1,5 +1,7 @@
 package io.github.s7i.doer.command
 
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import io.github.s7i.doer.Globals
 import io.github.s7i.doer.domain.kafka.KafkaConsumerFactory
 import io.github.s7i.doer.domain.kafka.KafkaFactory
@@ -114,6 +116,42 @@ class KafkaFeederTest extends Specification {
         feeder.yaml = new File("src/test/resources/ingest-with-template-value-set-repeat.yml")
         expect:
         feeder.onExecuteCommand()
+    }
+
+    def "template with RANDOM value set"() {
+        given:
+        def gson = new Gson();
+        def records = []
+        def producer = Mock(Producer) {
+            13 * send(_, _) >> { args ->
+                records << args[0]
+                Callback cb = args[1]
+                cb.onCompletion(new RecordMetadata(
+                        new TopicPartition("t", 0),
+                        0,
+                        records.size(),
+                        System.currentTimeMillis(),
+                        0L,
+                        10,
+                        10
+                ), null)
+            }
+            1 * close()
+        }
+        def producerFactory = Mock(KafkaProducerFactory) {
+            createProducer(_, _) >> producer
+        }
+        def feeder = new KafkaFeeder()
+        Globals.INSTANCE.kafka = new KafkaFactory(producerFactory, Mock(KafkaConsumerFactory))
+        feeder.yaml = new File("src/test/resources/ingest/template-with-random.yml")
+        expect:
+        feeder.onExecuteCommand()
+        records.forEach { rec ->
+            def str = new String(rec.value())
+            def nbr =  gson.fromJson(str, JsonObject.class).get("temperature").getAsDouble()
+            assert nbr > -31d
+            assert nbr < 61d
+        }
     }
 
 }
