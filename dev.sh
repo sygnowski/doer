b3f8060ac58c273303b7c4df2736a5ddbea6e964
mario <mario@dwarf.syg>
2025-02-04 18:56:30 +0100

    [REWORK] supporting kafka as storage

--- dev.sh
+++ dev.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 function doer() {
-  java -jar "./build/libs/doer-$(cat ./version)-all.jar" "$@"
+  if [[ "x${DEBUG}" == "x1" ]]; then
+    set -x
+    local LOCAL_DOER_JVM_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+  fi
+
+  java ${LOCAL_DOER_JVM_OPTS} -jar "./build/libs/doer-$(cat ./version)-all.jar" "$@"
 }
--- src/main/java/io/github/s7i/doer/command/Meshtastic.java
+++ src/main/java/io/github/s7i/doer/command/Meshtastic.java
@@ -1,29 +1,37 @@
 package io.github.s7i.doer.command;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.UnknownFieldSet;
 import io.github.s7i.doer.ConsoleLog;
+import io.github.s7i.doer.domain.kafka.KafkaConfig;
+import io.github.s7i.doer.domain.kafka.KafkaFactory;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.WorkerExecutor;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import picocli.CommandLine;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import picocli.CommandLine.Command;
 import picocli.CommandLine.Mixin;
 import picocli.CommandLine.Option;
 
-import java.util.Base64;
-import java.util.concurrent.TimeUnit;
-
-import static java.util.Objects.requireNonNull;
-
-@CommandLine.Command(name = "meshradio")
+@Command(name = "meshradio")
 public class Meshtastic extends VerticleCommand {
 
-    public static class Options {
+    @Slf4j
+    public static class Options implements KafkaConfig {
 
         @Option(names = "-p", description = "port", defaultValue = "80")
         int port;
@@ -31,6 +39,21 @@ public class Meshtastic extends VerticleCommand {
         String host;
         @Option(names = "--interval", description = "Api call interval.", defaultValue = "5")
         int interval;
+        @Option(names = "--kafka-config", description = "Kafka properties file.")
+        String kafkaConfig;
+        @Option(names = "--kafka-topic", defaultValue = "meshtastic-from-radio")
+        String kafkaTopic;
+
+        @Override
+        public String getKafkaPropFile() {
+            log.debug("using config file: {}", kafkaConfig);
+            return requireNonNull(kafkaConfig);
+        }
+
+        @Override
+        public Map<String, String> getKafka() {
+            return Map.of();
+        }
     }
 
     @Mixin
@@ -45,15 +68,22 @@ public class Meshtastic extends VerticleCommand {
     @Slf4j
     public static class MeshWebClient extends AbstractVerticle implements ConsoleLog {
 
+        public static final String API_FROM_RADIO = "/api/v1/fromradio?all=false";
         private final Options options;
         private WebClient client;
+        private WorkerExecutor executor;
+        private Producer<String, byte[]> producer;
 
         @Override
         public void init(Vertx vertx, Context context) {
             super.init(vertx, context);
 
-            var options = new WebClientOptions();
-            client = WebClient.create(vertx, options);
+            var clientOptions = new WebClientOptions();
+            client = WebClient.create(vertx, clientOptions);
+            executor = vertx.createSharedWorkerExecutor("async-op");
+            if (options.kafkaConfig != null) {
+                producer = initKafkaProducer();
+            }
         }
 
         @Override
@@ -62,25 +92,42 @@ public class Meshtastic extends VerticleCommand {
         }
 
         void callRadio(Long t) {
-            var req = client.get(options.port, options.host, "/api/v1/fromradio?all=false");
-            req.putHeader("Content-Type", "application/x-protobuf");
+            var req = client.get(options.port, options.host, API_FROM_RADIO);
             req.send(rep -> {
                 if (rep.succeeded()) {
-                    if (rep.result().statusCode() == 200) {
-                        var payload = rep.result().body().getBytes();
-                        if (payload.length > 0) {
-                            info("---\n{}\n---", Base64.getEncoder().encodeToString(payload));
-                            try {
-                                info("Proto: \n{}", UnknownFieldSet.parseFrom(payload));
-                            } catch (InvalidProtocolBufferException bpe) {
-                                log.warn("cannot parse proto: {}", payload);
-                            }
+                    var result = rep.result();
+                    if (result.statusCode() == 200) {
+                        var body = result.body();
+                        if (body == null) {
+                            return;
+                        }
+                        var payload = body.getBytes();
+                        info("---\n{}\n---", Base64.getEncoder().encodeToString(payload));
+
+                        if (options.kafkaConfig != null) {
+                            publishToKafka(payload).onSuccess(meta -> log.info("payload published: {}", meta));
+                        }
+
+                        try {
+                            info("Proto: \n{}", UnknownFieldSet.parseFrom(payload));
+                        } catch (InvalidProtocolBufferException bpe) {
+                            log.warn("cannot parse proto: {}", payload);
                         }
                     }
                 } else {
                     log.error("oops", rep.cause());
                 }
             });
+        }
+
+        Future<RecordMetadata> publishToKafka(byte[] data) {
+            return executor.executeBlocking(() -> producer.send(new ProducerRecord<>(options.kafkaTopic, data)).get());
+        }
+
+        private Producer<String, byte[]> initKafkaProducer() {
+            return new KafkaFactory()
+                  .getProducerFactory()
+                  .createProducer(options, false);
         }
     }
 }
