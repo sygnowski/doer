c125274df408bdeb016e23e0bba9b572c7ecf416
mario <mario@dwarf.syg>
2025-02-14 14:10:44 +0100

    development script improvements

--- dev.sh
+++ dev.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Local development script
+# Usage: $ . ./dev.sh && doer misc info
+
 function doer() {
-  java -jar "./build/libs/doer-$(cat ./version)-all.jar" "$@"
+  local OPTS="-Xms128M -Xmx512M -XX:+UnlockExperimentalVMOptions -XX:+UseZGC"
+  local LOGBACK="container-logback.xml"
+    if [[ "x${PLAIN}" == "x1" ]]; then
+      local LOGBACK="console.xml"
+    fi
+  local OPTS="-Dlogback.configurationFile=./src/main/resources/${LOGBACK}"
+  if [[ "x${DEBUG}" == "x1" ]]; then
+    local OPTS="${OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+  fi
+
+  java ${OPTS} -jar "./build/libs/doer-$(cat ./version)-all.jar" "$@"
 }
