82e31901e2d3dde065d43bf672fc137bc895f761
mario <mario@dwarf.syg>
2025-02-14 11:35:05 +0100

    dev stuff improvements

--- dev.sh
+++ dev.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# Local development script
+# Usage: $ . ./dev.sh && doer misc info
+
 function doer() {
+  local LOCAL_DOER_JVM_OPTS="-Dlogback.configurationFile=./src/main/resources/container-logback.xml"
   if [[ "x${DEBUG}" == "x1" ]]; then
-    set -x
-    local LOCAL_DOER_JVM_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+    LOCAL_DOER_JVM_OPTS="${LOCAL_DOER_JVM_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
   fi
 
   java ${LOCAL_DOER_JVM_OPTS} -jar "./build/libs/doer-$(cat ./version)-all.jar" "$@"
