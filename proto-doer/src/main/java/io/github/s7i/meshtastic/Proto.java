c877f474a5ef127e8036c965c6a51c2f5d6f0479
nefro85 <mariusz.sygnowski@gmail.com>
2025-04-18 13:44:49 +0200

    decoder other proto format

--- proto-doer/src/main/java/io/github/s7i/meshtastic/Proto.java
+++ proto-doer/src/main/java/io/github/s7i/meshtastic/Proto.java
@@ -13,19 +13,18 @@ import com.geeksville.mesh.MeshProtos.User;
 import com.geeksville.mesh.TelemetryProtos;
 import com.geeksville.mesh.TelemetryProtos.Telemetry;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import com.google.protobuf.TypeRegistry;
+import com.google.protobuf.UnknownFieldSet;
 import com.google.protobuf.util.JsonFormat;
 import com.google.protobuf.util.JsonFormat.Printer;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Gateway class to Meshtastic Protobuf classes.
@@ -38,6 +37,16 @@ public enum Proto {
           .add(TelemetryProtos.getDescriptor().getMessageTypes())
           .build());
     private final Gson gson = new Gson();
+
+    private final Map<Long, String> nodeNameMap = new ConcurrentHashMap<>();
+
+    private final Double[] myLocation = Optional.ofNullable(System.getenv("MY_LOC"))
+          .map(myLoc -> {
+              var myLatLLong = myLoc.split("\\:");
+              var myLat = Double.parseDouble(myLatLLong[0]);
+              var myLong = Double.parseDouble(myLatLLong[1]);
+              return new Double[]{myLat, myLong};
+          }).orElse(new Double[0]);
 
     public interface ToText {
 
@@ -98,6 +107,13 @@ public enum Proto {
 
             if (fromRadio.getPayloadVariantCase() == PayloadVariantCase.PACKET) {
                 jsonifyPacket(fromRadio.getPacket(), root);
+
+            } else if (fromRadio.getPayloadVariantCase() == PayloadVariantCase.NODE_INFO) {
+                var nodeInfo = fromRadio.getNodeInfo();
+
+                long id = nodeInfo.getNum() & 0xffffffffL;
+
+                nodeNameMap.put(id, nodeInfo.getUser().getShortName() + " / " + nodeInfo.getUser().getLongName());
             }
             return gson.toJson(root);
 
@@ -110,6 +126,10 @@ public enum Proto {
         var extJson = new JsonObject();
 
         root.add("ext", extJson);
+
+        Optional.ofNullable(nodeNameMap.get(packet.getFrom() & 0xffffffffL)).ifPresent(name -> {
+            extJson.addProperty("nodeFullName", name);
+        });
 
         var sec = packet.getRxTime();
         if (sec > 0) {
@@ -125,25 +145,26 @@ public enum Proto {
         signalQuality.addProperty("signal", SignalQuality.determineSignalQuality(packet.getRxSnr(), packet.getRxRssi()).toString());
         extJson.add("signalQuality", signalQuality);
 
+        if (!packet.hasDecoded()) {
+            return;
+        }
+
         var decoded = packet.getDecoded();
         try {
-            AtomicReference<JsonElement> distance = new AtomicReference<>();
-            Message unroll = switch (decoded.getPortnum()) {
+            var unroll = switch (decoded.getPortnum()) {
                 case POSITION_APP -> {
                     var pos = Position.parseFrom(decoded.getPayload());
-
-                    Optional.ofNullable(System.getenv("MY_LOC")).ifPresent(myLoc -> {
-                        var myLatLLong = myLoc.split("\\:");
-                        var myLat = Double.parseDouble(myLatLLong[0]);
-                        var myLong = Double.parseDouble(myLatLLong[1]);
+                    if (myLocation.length == 2) {
+                        var myLat = myLocation[0];
+                        var myLong = myLocation[1];
 
                         var posLat = pos.getLatitudeI() * 1e-7;
                         var posLong = pos.getLongitudeI() * 1e-7;
 
                         var kmUnit = GeoUtils.latLongToMeter(myLat, myLong, posLat, posLong) / 1000;
 
-                        distance.set(new JsonPrimitive(String.format("%.1f km", kmUnit)));
-                    });
+                        extJson.addProperty("distance", String.format("%.1f km", kmUnit));
+                    }
                     yield pos;
                 }
                 case TELEMETRY_APP -> Telemetry.parseFrom(decoded.getPayload());
@@ -151,12 +172,13 @@ public enum Proto {
                 case ROUTING_APP -> Routing.parseFrom(decoded.getPayload());
                 case NODEINFO_APP -> User.parseFrom(decoded.getPayload());
                 case TRACEROUTE_APP -> RouteDiscovery.parseFrom(decoded.getPayload());
-                default -> null;
+                default -> {
+                    var msg = UnknownFieldSet.parseFrom(decoded.getPayload());
+                    extJson.addProperty("other", msg.toString());
+                    yield null;
+                }
             };
             if (unroll != null) {
-                if (distance.get() != null) {
-                    extJson.add("distance", distance.get());
-                }
                 var unrollJson = gson.fromJson(printer.print(unroll), JsonObject.class);
                 unrollJson.keySet().forEach(key -> extJson.add(key, unrollJson.get(key)));
             }
