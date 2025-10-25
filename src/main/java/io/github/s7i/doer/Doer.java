f21817867a119b58470595b7b8485b03344d132a
nefro85 <mariusz.sygnowski@gmail.com>
2025-10-25 16:04:24 +0200

    [REWORK] loading subcommands, not working :/

--- src/main/java/io/github/s7i/doer/Doer.java
+++ src/main/java/io/github/s7i/doer/Doer.java
@@ -1,23 +1,11 @@
 package io.github.s7i.doer;
 
-import io.github.s7i.doer.command.GrpcHealth;
-import io.github.s7i.doer.command.Helix;
-import io.github.s7i.doer.command.KafkaFeeder;
-import io.github.s7i.doer.command.Meshtastic;
-import io.github.s7i.doer.command.MqttCommand;
-import io.github.s7i.doer.command.ProtoProcessor;
-import io.github.s7i.doer.command.Rocks;
-import io.github.s7i.doer.command.TcpCommand;
-import io.github.s7i.doer.command.ZooSrv;
-import io.github.s7i.doer.command.dump.KafkaDump;
 import io.github.s7i.doer.command.util.CommandManifest;
-import io.github.s7i.doer.command.util.HmacCommand;
-import io.github.s7i.doer.command.util.Misc;
 import io.github.s7i.doer.domain.ServiceEntrypoint;
 import io.github.s7i.doer.domain.grpc.GrpcServer;
-import io.github.s7i.doer.pipeline.PipelineService;
 import io.github.s7i.doer.util.Banner;
 import io.github.s7i.doer.util.GitProps;
+import io.github.s7i.doer.util.Utils;
 import java.io.IOException;
 import java.util.Arrays;
 import org.slf4j.Logger;
@@ -25,21 +13,7 @@ import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(name = "doer", description = "let's do big things...", subcommands = {
-      KafkaFeeder.class,
-      KafkaDump.class,
-      ProtoProcessor.class,
-      Helix.class,
-      Rocks.class,
-      GrpcHealth.class,
-      ZooSrv.class,
-      PipelineService.class,
-      MqttCommand.class,
-      Misc.class,
-      HmacCommand.class,
-      Meshtastic.class,
-      TcpCommand.class
-})
+@Command(name = "doer", description = "let's do big things...")
 public class Doer implements Runnable, Banner {
 
     public static final String DOER_CONSOLE = "doer.console";
@@ -92,7 +66,11 @@ public class Doer implements Runnable, Banner {
               ? new CommandManifest()
               : new Doer();
 
-        var exitCode = new CommandLine(command)
+        CommandLine commandLine = new CommandLine(command);
+        if (command instanceof Doer) {
+            Utils.loadCommand(commandLine::addSubcommand);
+        }
+        var exitCode = commandLine
               .setCaseInsensitiveEnumValuesAllowed(true)
               .execute(args);
 
